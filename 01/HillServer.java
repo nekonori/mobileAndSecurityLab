93b0e73cfc0b722862d336f6c4aad61a0c9306ac
Pragadesh BS <pragadeshbs@protonmail.com>
2023-08-20 09:08:35 +0530

    fix hill cipher

--- 01/HillServer.java
+++ 01/HillServer.java
@@ -3,150 +3,224 @@ import java.io.*;
 import java.net.*;
 
 public class HillServer {
+    int keymatrix[][];
+    int linematrix[];
+    int resultmatrix[];
+
+    public void divide(String temp, int s) {
+        while (temp.length() > s) {
+            String sub = temp.substring(0, s);
+            temp = temp.substring(s, temp.length());
+            perform(sub);
+        }
+        if (temp.length() == s)
+            perform(temp);
+        else if (temp.length() < s) {
+            for (int i = temp.length(); i < s; i++)
+                temp = temp + 'x';
+            perform(temp);
+        }
+    }
+
+    public void perform(String line) {
+        linetomatrix(line);
+        linemultiplykey(line.length());
+        result(line.length());
+    }
+
+    public void keytomatrix(String key, int len) {
+        keymatrix = new int[len][len];
+        int c = 0;
+        for (int i = 0; i < len; i++) {
+            for (int j = 0; j < len; j++) {
+                keymatrix[i][j] = ((int) key.charAt(c)) - 97;
+                c++;
+            }
+        }
+    }
+
+    public void linetomatrix(String line) {
+        linematrix = new int[line.length()];
+        for (int i = 0; i < line.length(); i++) {
+            linematrix[i] = ((int) line.charAt(i)) - 97;
+        }
+    }
+
+    public void linemultiplykey(int len) {
+        resultmatrix = new int[len];
+        for (int i = 0; i < len; i++) {
+            for (int j = 0; j < len; j++) {
+                resultmatrix[i] += keymatrix[i][j] * linematrix[j];
+            }
+            resultmatrix[i] %= 26;
+        }
+    }
+
+    public void result(int len) {
+        String result = "";
+        for (int i = 0; i < len; i++) {
+            result += (char) (resultmatrix[i] + 97);
+        }
+        System.out.print(result);
+    }
+
+    public boolean check(String key, int len) {
+        keytomatrix(key, len);
+        int d = determinant(keymatrix, len);
+        d = d % 26;
+        if (d == 0) {
+            System.out
+                    .println("Invalid key!!! Key is not invertible because determinant=0...");
+            return false;
+        } else if (d % 2 == 0 || d % 13 == 0) {
+            System.out
+                    .println("Invalid key!!! Key is not invertible because determinant has common factor with 26...");
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    public int determinant(int A[][], int N) {
+        int res;
+        if (N == 1)
+            res = A[0][0];
+        else if (N == 2) {
+            res = A[0][0] * A[1][1] - A[1][0] * A[0][1];
+        } else {
+            res = 0;
+            for (int j1 = 0; j1 < N; j1++) {
+                int m[][] = new int[N - 1][N - 1];
+                for (int i = 1; i < N; i++) {
+                    int j2 = 0;
+                    for (int j = 0; j < N; j++) {
+                        if (j == j1)
+                            continue;
+                        m[i - 1][j2] = A[i][j];
+                        j2++;
+                    }
+                }
+                res += Math.pow(-1.0, 1.0 + j1 + 1.0) * A[0][j1]
+                        * determinant(m, N - 1);
+            }
+        }
+        return res;
+    }
+
+    public void cofact(int num[][], int f) {
+        int b[][], fac[][];
+        b = new int[f][f];
+        fac = new int[f][f];
+        int p, q, m, n, i, j;
+        for (q = 0; q < f; q++) {
+            for (p = 0; p < f; p++) {
+                m = 0;
+                n = 0;
+                for (i = 0; i < f; i++) {
+                    for (j = 0; j < f; j++) {
+                        b[i][j] = 0;
+                        if (i != q && j != p) {
+                            b[m][n] = num[i][j];
+                            if (n < (f - 2))
+                                n++;
+                            else {
+                                n = 0;
+                                m++;
+                            }
+                        }
+                    }
+                }
+                fac[q][p] = (int) Math.pow(-1, q + p) * determinant(b, f - 1);
+            }
+        }
+        trans(fac, f);
+    }
+
+    void trans(int fac[][], int r) {
+        int i, j;
+        int b[][], inv[][];
+        b = new int[r][r];
+        inv = new int[r][r];
+        int d = determinant(keymatrix, r);
+        int mi = mi(d % 26);
+        mi %= 26;
+        if (mi < 0)
+            mi += 26;
+        for (i = 0; i < r; i++) {
+            for (j = 0; j < r; j++) {
+                b[i][j] = fac[j][i];
+            }
+        }
+        for (i = 0; i < r; i++) {
+            for (j = 0; j < r; j++) {
+                inv[i][j] = b[i][j] % 26;
+                if (inv[i][j] < 0)
+                    inv[i][j] += 26;
+                inv[i][j] *= mi;
+                inv[i][j] %= 26;
+            }
+        }
+        // System.out.println("\nInverse key:");
+        matrixtoinvkey(inv, r);
+    }
+
+    public int mi(int d) {
+        int q, r1, r2, r, t1, t2, t;
+        r1 = 26;
+        r2 = d;
+        t1 = 0;
+        t2 = 1;
+        while (r1 != 1 && r2 != 0) {
+            q = r1 / r2;
+            r = r1 % r2;
+            t = t1 - (t2 * q);
+            r1 = r2;
+            r2 = r;
+            t1 = t2;
+            t2 = t;
+        }
+        return (t1 + t2);
+    }
+
+    public void matrixtoinvkey(int inv[][], int n) {
+        String invkey = "";
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                invkey += (char) (inv[i][j] + 97);
+            }
+        }
+        // System.out.print(invkey);
+    }
+
     public static void main(String[] args) throws Exception {
+        HillServer obj = new HillServer();
         Scanner sc = new Scanner(System.in);
         ServerSocket ss = new ServerSocket(4000);
-        Socket s = ss.accept();
-        PrintWriter out = new PrintWriter(s.getOutputStream(), true);
-        BufferedReader in = new BufferedReader(new InputStreamReader(s.getInputStream()));
+        Socket socket = ss.accept();
+        PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+        BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
         String ipStr = "";
         while (!ipStr.equals("stop")) {
             ipStr = in.readLine();
-            int stringLength = Integer.parseInt(in.readLine());
             System.out.println("Message from client: " + ipStr);
-            System.out.println("Decrypted message: \t" + HillCipher(ipStr, "GYBNQKURP").substring(0, stringLength));
+            String key = "jgrd";
+            double sq = Math.sqrt(key.length());
+            if (sq != (long) sq)
+                System.out
+                        .println("Invalid key length!!! Does not form a square matrix...");
+            else {
+                int s = (int) sq;
+                if (obj.check(key, s)) {
+                    System.out.print("Decrypted message: ");
+                    obj.divide(ipStr, s);
+                    obj.cofact(obj.keymatrix, s);
+                }
+            }
         }
         in.close();
         out.close();
-        s.close();
+        socket.close();
         ss.close();
         sc.close();
-    }
-
-    static void getKeyMatrix(String key, int keyMatrix[][]) {
-        int k = 0;
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                keyMatrix[i][j] = (key.charAt(k)) % 65;
-                k++;
-            }
-        }
-    }
-
-    static void decrypt(int ptMatrix[][],
-            int inverseKeyMatrix[][],
-            int messageVector[][]) {
-        int x, i, j;
-        for (i = 0; i < 3; i++) {
-            for (j = 0; j < 1; j++) {
-                ptMatrix[i][j] = 0;
-                for (x = 0; x < 3; x++) {
-                    ptMatrix[i][j] += inverseKeyMatrix[i][x] * messageVector[x][j];
-                }
-
-                ptMatrix[i][j] = ptMatrix[i][j] % 26;
-            }
-        }
-    }
-
-    static void getCofactor(int A[][], int temp[][], int p, int q, int n) {
-        int i = 0, j = 0;
-        for (int row = 0; row < n; row++) {
-            for (int col = 0; col < n; col++) {
-                if (row != p && col != q) {
-                    temp[i][j++] = A[row][col];
-                    if (j == n - 1) {
-                        j = 0;
-                        i++;
-                    }
-                }
-            }
-        }
-    }
-
-    static int determinant(int A[][], int n) {
-        int D = 0;
-
-        if (n == 1)
-            return A[0][0];
-
-        int[][] temp = new int[3][3];
-
-        int sign = 1;
-
-        for (int f = 0; f < n; f++) {
-            getCofactor(A, temp, 0, f, n);
-            D += sign * A[0][f] * determinant(temp, n - 1);
-            sign = -sign;
-        }
-
-        return D;
-    }
-
-    static void adjoint(int A[][], int[][] adj) {
-        int sign = 1;
-        int[][] temp = new int[3][3];
-
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                getCofactor(A, temp, i, j, 3);
-                sign = ((i + j) % 2 == 0) ? 1 : -1;
-                adj[j][i] = (sign) * (determinant(temp, 3 - 1));
-            }
-        }
-    }
-
-    static void getInverseMatrix(int[][] keyMatrix, int[][] inverseMatrix) {
-        adjoint(keyMatrix, inverseMatrix);
-    }
-
-    static String HillCipher(String message, String key) {
-        int[][] keyMatrix = new int[3][3];
-        getKeyMatrix(key, keyMatrix);
-
-        int[][] inverseMatrix = new int[3][3];
-        getInverseMatrix(keyMatrix, inverseMatrix);
-
-        int det = determinant(keyMatrix, 3) % 26;
-
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                inverseMatrix[i][j] = (inverseMatrix[i][j] * det) % 26;
-                if (inverseMatrix[i][j] < 0)
-                    inverseMatrix[i][j] += 26;
-            }
-        }
-
-        int blockSize = 3;
-        int no_of_blocks = message.length() / blockSize;
-        if (message.length() % 3 > 0)
-            no_of_blocks++;
-
-        int[][][] cipherMatrix = new int[no_of_blocks][3][1];
-
-        StringBuilder plainText = new StringBuilder();
-
-        for (int block = 0; block < no_of_blocks; block++) {
-            for (int i = 0; i < blockSize; i++) {
-                int curIndex = i + block * blockSize;
-                int c;
-                if (curIndex < message.length())
-                    c = (message.charAt(curIndex)) % 65;
-                else
-                    c = ' ';
-                cipherMatrix[block][i][0] = c;
-            }
-
-            int[][] ptMatrix = new int[3][1];
-
-            decrypt(ptMatrix, inverseMatrix, cipherMatrix[block]);
-
-            for (int i = 0; i < ptMatrix.length; i++)
-                ptMatrix[i][0] %= 26;
-
-            for (int i = 0; i < 3; i++)
-                plainText.append((char) (ptMatrix[i][0] + 65));
-        }
-        return plainText.toString();
     }
 }
