984a561c50961be9afaad562e9264fe338bea60f
Pragadesh BS <pragadeshbs@protonmail.com>
2023-10-09 21:02:03 +0530

    add exp 8

---
+++ androidStudio/eventHandling/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val clickMe = findViewById<Button>(R.id.click_me)
+
+        clickMe.setOnClickListener{
+            Toast.makeText(this, "Button Clicked", Toast.LENGTH_SHORT).show()
+        }
+    }
+}
---
+++ androidStudio/notification/myapplication/Counter.kt
@@ -0,0 +1,5 @@
+package com.example.myapplication
+
+object Counter {
+    var value = 0
+}
